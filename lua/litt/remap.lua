a7f0ed6059098023e19fd2e4ccd9c4d81eb18c89
litt <kittnablude@gmail.com>
2025-05-25 10:01:41 +0300

    .

--- lua/litt/remap.lua
+++ lua/litt/remap.lua
@@ -30,3 +30,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<ESC>", ":noh<CR>")
+
+vim.keymap.set("n", "<C-s>", ":w<CR>")
